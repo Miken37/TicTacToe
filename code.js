285c135ccb53d3eb25e44980570a3f3c4e4b194a
Miken37 <mike-166@hotmail.com>
2022-10-28 06:41:18 -0400

    Functionality of buttons and placement of x and o works along with turn prompt. Started working on JS functionality for game logic

--- code.js
+++ code.js
@@ -1,1 +1,38 @@
-console.log("Hit");
+let squares = document.querySelector(".main-container");
+let resetButton = document.querySelector(".reset");
+let squareTest = squares.childNodes;
+let playerOne = true;
+
+let currentPlayer = document.querySelector(".playerTurn");
+
+function playerCheck() {
+    if (playerOne) {
+        playerOne = false;
+        currentPlayer.style.color = "yellow";
+        currentPlayer.innerHTML = "Player Two";
+        return 'url(cross.png)';
+        
+    } else{
+        playerOne = true;
+        currentPlayer.style.color = "rgb(135, 201, 36)";
+        currentPlayer.innerHTML = "Player One";
+        return 'url(circle.png)';
+        
+    };
+}
+
+
+
+squareTest.forEach((element) => {
+    element.addEventListener("click", () => {
+        element.style.backgroundImage = playerCheck();
+            
+    })
+})
+
+resetButton.addEventListener("click", () =>{
+    squareTest.forEach((element => {
+        element.style.backgroundImage = "";
+    }))
+})
+
